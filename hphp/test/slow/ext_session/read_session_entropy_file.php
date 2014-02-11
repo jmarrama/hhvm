3adf48a433189b5e8f9510840635dfb7b11e1fac
Joe Marrama <jmarrama@box.com>
2014-02-11 13:10:23 -0800

    Fixed the session extension to call the right version of open() when reading in the optional entropy file

---
+++ hphp/test/slow/ext_session/read_session_entropy_file.php
@@ -0,0 +1,32 @@
+<?php
+
+ob_start();
+
+/* 
+ * Prototype : string session_id([string $id])
+ * Description : Get and/or set the current session id
+ * Source code : ext/session/session.c 
+ */
+
+echo "*** Testing session_id() : variation ***\n";
+
+$directory = dirname(__FILE__);
+$filename = ($directory."/entropy.txt");
+ini_set("session.entropy_file", $filename);
+file_put_contents($filename, "Hello World!");
+ini_set("session.entropy_length", filesize($filename));
+ini_set("session.hash_function", 0);
+
+var_dump(session_start());
+var_dump(session_id());
+var_dump(session_destroy());
+
+ini_set("session.hash_function", 1);
+var_dump(session_start());
+var_dump(session_id());
+var_dump(session_destroy());
+var_dump(unlink($filename));
+
+echo "Done";
+ob_end_flush();
+?>
